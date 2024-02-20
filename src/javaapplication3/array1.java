203db56cc4cbe77c9b73c2cd423809a8077aa47e
Gurkirat Singh Chumber <chumbgur@sheridancollege.ca>
2024-02-20 09:07:53 -0500

    created a new class

---
+++ src/javaapplication3/array1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author gur28
+ */
+public class array1 {
+    public static void main(String args[]){
+        String a[] = {"a","b","c","d"};
+    }
+     
+}
